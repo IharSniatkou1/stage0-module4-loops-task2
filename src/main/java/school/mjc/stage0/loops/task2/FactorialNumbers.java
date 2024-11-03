f2adc179165890a4f5903ec7d9dc3b199c3d4996
Ihar Sniatkou <iharsniatkou@gmail.com>
2024-11-03 17:13:00 +0100

    Stage0-module4-loops-task2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int i = 0;
+        int factorial = 1;
 
+        while (i <= printToInclusive) {
+            if (i == 0) {
+                factorial = 1; // 0! is 1
+            } else {
+                factorial *= i; // Calculate i! based on (i-1)! * i
+            }
+            System.out.println(factorial);
+            i++;
+        }
     }
 }
